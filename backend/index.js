51f39d0003ff6b0885d3ef8924dca06cdde9ba47
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2024-10-30 18:00:00 +0530

    builded app with required installation and connection to database

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const { MONGO_URL, PORT } = process.env;
+const app = express();  //represents express application
+
+mongoose
+.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>console.log("mongodb connected successfully"))
+.catch(()=> console.error(err));
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on port ${PORT}`);
+})
+
+app.use(
+    cors({
+      origin: ["http://localhost:4000"],          //allows req from this port only
+      methods: ["GET", "POST", "PUT", "DELETE"],  //allows http req
+      credentials: true,                         //allows cookies and credentials to be sent with requests
+    })
+  );
+
+  app.use(express.json());      //middleware function which parses json correctly, req.body will allow you to access this data
---
+++ frontend/client/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellooo</h1>
+    </div>
+  );
+}
+
+export default App;
